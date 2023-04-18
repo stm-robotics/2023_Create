90c426786dbd292ce8e8921de982745910ef20f4
stm-robotics <124404090+stm-robotics@users.noreply.github.com>
2023-04-18 17:50:57 -0500

    Initial Code Upload

---
+++ Eli.h
@@ -0,0 +1,23 @@
+void pickuppoms(int servo);
+void pickuppoms(int servo){    
+   int x=1;
+    // this is to collect all the poms and to score us all the points. If this fails we lose.
+
+ 
+ while(x<5)
+   {
+        
+       enable_servos();
+       motor(1,70);
+       motor(0,70);
+      msleep(1500);
+       printf("stop 1");
+    set_servo_position(0,481);
+      motor(01,50);
+       motor(0,50);
+    msleep(1500);
+    set_servo_position(0,1440);
+    x=x+1;
+}
+
+}
---
+++ main.c
@@ -0,0 +1,15 @@
+#include <kipr/wombat.h>
+#include <Gracie.h>
+#include <Eli.h>
+#include <Matthew.h>
+#include <Noah.h>
+#include <Theresa.h>
+const int RM=0;//right motor
+const int LM=1;//left motor
+const int SR=1;//servo
+const int TP=0;//top hat
+int main()
+{
+    pickuppoms(0);
+    return 0;
+}
